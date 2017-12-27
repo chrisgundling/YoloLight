1c9a5835a2944ebc11d1519cef4621cb5a8ad4bc
chris gundling <chrisgundling@gmail.com>
2017-12-27 14:58:58 -0800

    Update darkflow.py

--- scripts/darkflow.py
+++ scripts/darkflow.py
@@ -11,19 +11,16 @@ def process(model, img):
     result = model.return_predict(img[None, :, :, :])
     return result
 
-
 def getModel():
     options = {"model": "./cfg/tiny-yolo-udacity.cfg", "backup": "./ckpt/","load": 8987, "gpu": 1.0}
     model = TFNet(options)
     return model
 
-
 def main():
     parser = argparse.ArgumentParser(description='Script for running yolo_light node')
     args = parser.parse_args()
-    node = TLightNode(lambda: get_model(), process)
+    node = TLightNode(lambda: getModel(), process)
     rospy.spin()
-
 
 if __name__ == '__main__':
     main()
